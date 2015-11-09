147c8a9a6ec7946a88bb27459080080f3c51eb16
FA12-BSE-103 <FA12-BSE-103@LAB7A-01.cns.ciit>
2015-11-09 09:41:50 +0500

    fddfd

---
+++ src/gihub/Myclass2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gihub;
+
+/**
+ *
+ * @author FA12-BSE-103
+ */
+public class Myclass2 {
+    
+    
+            
+}
